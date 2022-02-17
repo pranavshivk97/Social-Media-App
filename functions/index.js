3a9454391791bf1cec6b8a4a5a37a7fd3835efe0
Pranav Shivkumar <pranavshivkumar3@gmail.com>
2022-02-17 18:01:55 -0500

    delete screams with auth

--- functions/index.js
+++ functions/index.js
@@ -2,7 +2,7 @@ const functions = require("firebase-functions");
 
 const app = require('express')();
 
-const { getAllScreams, postOneScream, getScream, commentOnScream } = require('./handlers/screams')
+const { getAllScreams, postOneScream, getScream, commentOnScream, likeScream, unlikeScream, deleteScream } = require('./handlers/screams')
 const { register, login, uploadImage, addUserDetails, getAuthenticatedUser } = require('./handlers/users')
 
 const fbAuth  = require('./util/fbAuth')
@@ -12,8 +12,11 @@ app.get('/screams', getAllScreams);
 app.post('/scream', fbAuth, postOneScream);
 app.get('/scream/:screamId', getScream);
 // TODO: delete scream
+app.delete('/scream/:screamId', fbAuth, deleteScream);
 // TODO: like a scream
+app.get('/scream/:screamId/like', fbAuth, likeScream);
 // TODO: unlike scream
+app.get('/scream/:screamId/unlike', fbAuth, unlikeScream);
 // TODO: comment on scream
 app.post('/scream/:screamId/comment', fbAuth, commentOnScream);
 
