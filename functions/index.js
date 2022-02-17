c1f18fe80f2c3bc19d8787afaa7de6ada845315c
Pranav Shivkumar <pranavshivkumar3@gmail.com>
2022-02-17 17:18:02 -0500

    add comments to scream

--- functions/index.js
+++ functions/index.js
@@ -2,7 +2,7 @@ const functions = require("firebase-functions");
 
 const app = require('express')();
 
-const { getAllScreams, postOneScream } = require('./handlers/screams')
+const { getAllScreams, postOneScream, getScream, commentOnScream } = require('./handlers/screams')
 const { register, login, uploadImage, addUserDetails, getAuthenticatedUser } = require('./handlers/users')
 
 const fbAuth  = require('./util/fbAuth')
@@ -10,6 +10,12 @@ const fbAuth  = require('./util/fbAuth')
 // Scream routes
 app.get('/screams', getAllScreams);
 app.post('/scream', fbAuth, postOneScream);
+app.get('/scream/:screamId', getScream);
+// TODO: delete scream
+// TODO: like a scream
+// TODO: unlike scream
+// TODO: comment on scream
+app.post('/scream/:screamId/comment', fbAuth, commentOnScream);
 
 // User routes 
 app.post('/register', register);
